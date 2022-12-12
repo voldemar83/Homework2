5d17acf9c773b284181ec937a3e2c6708c70c0da
Vladimir83 <vovaf@bk.ru>
2022-12-12 13:06:47 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = 0;
+int b = 0;
+
+Console.Write("Введите трехзначное число: ");
+a = int.Parse(Console.ReadLine() ?? "");
+
+b = a / 10 % 10;
+
+System.Console.WriteLine($"Вторая цифра числа {a} - это {b} ");
