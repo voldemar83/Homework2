2888c61f80ed875141210cdd98945971eccf1ba6
Vladimir83 <vovaf@bk.ru>
2022-12-12 13:19:26 +0300

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,11 @@
+﻿string Week(int data)
+{
+    if (data == 6 || data == 7)
+        return "day off";
+    else
+        return "weekday";           
+}
+
+Console.WriteLine("Input number:");
+int num = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine(Week(num));
