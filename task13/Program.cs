be3b060b9943034bd701c83e4ef4b445326e8111
Vladimir83 <vovaf@bk.ru>
2022-12-12 13:13:17 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,20 @@
+﻿string Number(int n)
+{
+    if (n < 100)
+        return "not have third number";
+    else if (n > 99 && n < 1000)
+        return $"Number is {n % 10}"; 
+    else
+    {
+        while (n > 1000)
+        {
+            n = n / 10;
+        }   
+    }
+        return $"Number is {n % 10}";  
+
+}
+
+Console.WriteLine("Input number:");
+int num = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine(Number(num));
